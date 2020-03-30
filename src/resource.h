3c752a739a51ef50784e4eb9375b20407c4ce102
Kevin <anyboo@qq.com>
2020-03-30 11:31:46 +0800

    Uniform Secure UI Initialization

---
+++ src/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 360Safe.rc
+
+// 新对象的下一组默认值
+// 
+
+#define STRINGIZE2(s)		#s
+#define STRINGIZE(s)        STRINGIZE2(s)
+
+#define MAIN				1 
+#define Major				0 
+#define Minor				0 
+#define FileIndex			1
+
+#define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
+#define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
+
+#define WMAIN				2 
+#define WMajor				0 
+#define WMinor				6 
+#define WFileIndex			79
+
+#define VTSPRODUCTVERSION			WMAIN,WMajor,WMinor,WFileIndex
+#define VTSPRODUCTVERSION_STR		STRINGIZE(WMAIN) "." STRINGIZE(WMajor) "." STRINGIZE(WMinor) "." STRINGIZE(WFileIndex)
+
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
