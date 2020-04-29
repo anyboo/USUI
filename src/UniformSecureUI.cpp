456d088f3eeea96f617cfe3a4f81935f89cd5697
Kevin <anyboo@qq.com>
2020-04-29 10:53:19 +0800

    Add Tab Switch Effect

--- src/UniformSecureUI.cpp
+++ src/UniformSecureUI.cpp
@@ -38,22 +38,18 @@ public:
 		{
 			CDuiString name = msg.pSender->GetName();
 			CTabLayoutUI* pControl = static_cast<CTabLayoutUI*>(m_pm.FindControl(_T("switch")));
-			if(name==_T("examine"))
+			if(name==_T("home"))
 				 pControl->SelectItem(0);
-			else if(name==_T("trojan"))
+			else if(name==_T("cert"))
 				 pControl->SelectItem(1);
-			else if(name==_T("plugins"))
+			else if(name==_T("stamper"))
 				pControl->SelectItem(2);
-			else if(name==_T("vulnerability"))
+			else if(name==_T("download"))
 				pControl->SelectItem(3);
-			else if(name==_T("rubbish"))
+			else if(name==_T("upgrade"))
 				pControl->SelectItem(4);
-			else if(name==_T("cleanup"))
+			else if(name==_T("help"))
 				pControl->SelectItem(5);
-			else if(name==_T("fix"))
-				pControl->SelectItem(6);
-			else if(name==_T("tool"))
-				pControl->SelectItem(7);
 		}
 	}
 
